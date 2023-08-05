a8726e8f3387db84440a1fa9518f7576c79fe142
Ray Carneiro <ray.silva@msn.com>
2023-08-05 13:41:25 +0000

    Updated providers.tf

--- src/iac/providers.tf
+++ src/iac/providers.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      #version = "~> 3.0.2"
-      version = "~>2.0"
+      version = "~> 3.0.2"
     }
     random = {
       source  = "hashicorp/random"
-      #version = "~>3.0"
-      version = "~>2.0"
+      version = "~>3.0"
     }
   }
 
